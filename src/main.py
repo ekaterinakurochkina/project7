661a5b4a2b98412a80be9a279e1e89b4dfbf3ac1
Katerina Kurochkina <katifedr@mail.ru>
2024-07-26 13:06:01 +0500

    main, test_main

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
 class Product:
     """Класс для представления продукта"""
+
     name: str
     description: str
     price: float
     quantity: int
+
     def __init__(self, name, description, price, quantity):
         """Метод для инициализации экземпляра класса. Задаем значения атрибутам экземпляра."""
         self.name = name
@@ -14,11 +16,13 @@ class Product:
 
 class Category(Product):
     """Класс для представления категории"""
+
     name: str
     description: str
     products: list
-    category_count = 0   # Переменная на уровне класса для подсчета количества категорий
-    product_count = 0   # Переменная на уровне класса для подсчета количества товаров
+    category_count = 0  # Переменная на уровне класса для подсчета количества категорий
+    product_count = 0  # Переменная на уровне класса для подсчета количества товаров
+
     def __init__(self, name, description, products):
         """Метод для инициализации экземпляра класса. Задаем значения атрибутам экземпляра."""
         self.name = name
@@ -28,9 +32,10 @@ class Category(Product):
         Category.product_count += len(products) if products else 0
 
 
-
 if __name__ == "__main__":
-    product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+    product1 = Product(
+        "Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5
+    )
     product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
     product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
 
@@ -49,9 +54,11 @@ if __name__ == "__main__":
     print(product3.price)
     print(product3.quantity)
 
-    category1 = Category("Смартфоны",
-                         "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
-                         [product1, product2, product3])
+    category1 = Category(
+        "Смартфоны",
+        "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+        [product1, product2, product3],
+    )
 
     print(category1.name == "Смартфоны")
     print(category1.description)
@@ -59,10 +66,12 @@ if __name__ == "__main__":
     print(category1.category_count)
     print(category1.product_count)
 
-    product4 = Product("55\" QLED 4K", "Фоновая подсветка", 123000.0, 7)
-    category2 = Category("Телевизоры",
-                         "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником",
-                         [product4])
+    product4 = Product('55" QLED 4K', "Фоновая подсветка", 123000.0, 7)
+    category2 = Category(
+        "Телевизоры",
+        "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником",
+        [product4],
+    )
 
     print(category2.name)
     print(category2.description)
---
+++ tests/test_main.py
@@ -0,0 +1,21 @@
+import pytest
+from src.main import Category, Product
+
+
+def test_product_init(product_cheese):
+    assert product_cheese.name == "cheese"
+    assert product_cheese.description == "Гауда"
+    assert product_cheese.price == 600.00
+    assert product_cheese.quantity == 150
+
+
+def test_category_init(category_food, category_toys):
+    assert category_food.name == "Food"
+    assert category_food.description == "Продукты питания"
+    assert len(category_food.products) == 3
+    assert category_toys.name == "Toys"
+    assert category_toys.description == "Детские игрушки"
+    assert len(category_toys.products) == 3
+
+    assert Category.category_count == 2
+    assert Category.product_count == 6
